f5e3219e1dbb7062b89e49086983b8cfd61ffc73
Loshitha <loshitha.e@papertrl.com>
2023-04-07 15:57:49 +0530

    fix login issue

--- src/app/services/custom-interceptor.service.ts
+++ src/app/services/custom-interceptor.service.ts
@@ -13,6 +13,12 @@ export class CustomInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    // Exclude JWT token for login requests
+    if (request.url.endsWith('/authenticate')) {
+      return next.handle(request);
+    }
+
     const localToken = localStorage.getItem('token');
     request = request.clone({headers:request.headers.set('Authorization','Bearer ' +localToken)})
     return next.handle(request);
